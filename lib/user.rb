c2bf01c7097f0ce78f11aded5e86abd3eadcf51c
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-03 23:51:24 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -5,4 +5,20 @@ class User
     @first_name = first_name
     @last_name = last_name
   end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+ 
+  def first_name
+    @first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+ 
+  def last_name
+    @last_name
+  end
 end
